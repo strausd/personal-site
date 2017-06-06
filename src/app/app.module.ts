6699824fa75fe552c2e88433021a5722cca8ab4d
Daniel Straus <strausd@strausd-MBA.T-mobile.com>
2017-06-05 21:59:10 -0500

    Add verification to form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +23,7 @@ import { ContactComponent } from './contact/contact.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  contact: FormGroup;
+  email: string = '';
+  confirmEmail: string = '';
+  message: string = '';
+
   constructor() { }
 
   ngOnInit() {
+    this.contact = new FormGroup({
+      email: new FormControl(this.email, [Validators.email, Validators.required]),
+      confirmEmail: new FormControl(this.confirmEmail, [Validators.email, Validators.required, this.mismatchEmails.bind(this)]),
+      message: new FormControl(this.message, [Validators.required, Validators.minLength(1), Validators.maxLength(500)])
+    });
+  }
+
+  onSubmit() {
+    this.email = this.contact.value.email;
+    this.confirmEmail = this.contact.value.confirmEmail;
+    this.message = this.contact.value.message;
+    // this.contact.reset();
+    console.log(this.contact);
+  }
+
+  mismatchEmails(control: FormControl): {[s: string]: boolean} {
+    if (this.email !== control.value) {
+      return {'mismatchEmails': true};
+    }
+    return null;
   }
 
 }
